a6dc8c8ef7c95e68a1a412a87e03b3d1df0e1cdd
ziyad-tarek1 <ziyadtarek180@gmail.com>
2025-01-24 14:16:58 +0200

    Create task6.py

---
+++ Day2/task6.py
@@ -0,0 +1,5 @@
+# 6-Write a script that iterates through a list of server names and prints each server name in uppercase:
+server_names = ['server1', 'server2', 'server3', 'server4']
+
+for server in server_names:
+    print(server.upper())
